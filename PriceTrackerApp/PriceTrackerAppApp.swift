b6e119d85945e8950fac2ece261794960722cea2
Christopher Nassar <christopher.nassar@totersapp.com>
2025-08-22 12:56:15 +0300

    Initial Commit

---
+++ PriceTrackerApp/PriceTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriceTrackerAppApp.swift
+//  PriceTrackerApp
+//
+//  Created by Christopher Nassar on 22/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PriceTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
